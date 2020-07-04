b2c06192077be7d92fb2d4065881cc10c4e6b891
Frank Moreno <frankmoreno1993@gmail.com>
2020-07-04 15:57:44 -0500

    v0.1.0

--- flutils/test/flutils_test.dart
+++ flutils/test/flutils_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutils/flutils.dart';
+//import 'package:flutter_test/flutter_test.dart';
+//import 'package:flutils/flutils.dart';
 
 void main() {
   //test('adds one to input values', () {
