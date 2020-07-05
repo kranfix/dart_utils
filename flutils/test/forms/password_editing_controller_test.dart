07334a16a52e98799241c697f33685682fe96dd5
Frank Moreno <frankmoreno1993@gmail.com>
2020-07-05 12:37:50 -0500

    Initialized PasswordEditingController test

--- flutils/test/forms/password_editing_controller_test.dart
+++ flutils/test/forms/password_editing_controller_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutils/flutils.dart';
 
 void main() {
-  group('Selection and Edition', () {
+  group('Selection and Edition for empty text', () {
     final controller = PasswordEditingController();
 
     test('verifies initial conditions', () {
@@ -25,6 +25,38 @@ void main() {
     test('edits for first time', () {
       controller.text = 'a';
       expect(controller.text, equals('a'));
+      expect(controller.wasSelected, equals(true));
+      expect(controller.wasNotSelected, equals(false));
+      expect(controller.isEdited, equals(true));
+      expect(controller.isNotEdited, equals(false));
+    });
+  });
+
+  group('Selection and Edition for initilized text', () {
+    const password = 'password';
+    final controller = PasswordEditingController(text: password);
+
+    test('verifies initial conditions', () {
+      expect(controller.text, equals(password));
+      expect(controller.wasSelected, equals(false));
+      expect(controller.wasNotSelected, equals(true));
+      expect(controller.isEdited, equals(false));
+      expect(controller.wasNotSelected, equals(true));
+    });
+
+    test('selects for first time', () {
+      controller.text = password;
+      expect(controller.text, equals(password));
+      expect(controller.wasSelected, equals(true));
+      expect(controller.wasNotSelected, equals(false));
+      expect(controller.isEdited, equals(false));
+      expect(controller.isNotEdited, equals(true));
+    });
+
+    test('edits for first time', () {
+      final newPassword = '${password}a';
+      controller.text = newPassword;
+      expect(controller.text, equals(newPassword));
       expect(controller.wasSelected, equals(true));
       expect(controller.wasNotSelected, equals(false));
       expect(controller.isEdited, equals(true));
