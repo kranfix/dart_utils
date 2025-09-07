2e9cc7c3f81f2264189dc957f3c501496616f407
Frank Moreno @kranfix <frankmoreno1993@gmail.com>
2025-09-07 01:40:34 -0500

    Slice and SliceMut

--- collection_commons/lib/src/slice.dart
+++ collection_commons/lib/src/slice.dart
@@ -1,29 +1,81 @@
 import 'dart:collection';
 
 import 'package:collection_commons/src/readonly_list/unmodificable_length_list.dart';
+import 'package:meta/meta.dart';
+
+/// A mutable view of a portion of another list.
+///
+/// Unlike [Slice], which is an immutable view, `SliceMut` allows you to
+/// modify the underlying list by assigning new values to its elements.
+/// All mutations are applied directly to the original list.
+extension type SliceMut<E>._(_SliceInner<E> _slice) implements _SliceInner<E> {
+  /// Creates a new mutable slice from an iterable.
+  ///
+  /// This factory constructor creates a new list from the `source` and
+  /// then wraps it in a `SliceMut`. Since it creates a copy, changes
+  /// will not affect the original source, but will be local to this slice.
+  SliceMut.from(Iterable<E> source) : this._(_SliceInner._([...source]));
+
+  /// Creates a new mutable slice that directly references a list.
+  ///
+  /// This constructor is "unsafe" because it does not create a copy of
+  /// the source list. Any changes to this slice will directly modify the
+  /// provided `source` list. Use with caution.
+  SliceMut.unsafeFrom(List<E> source) : this._(_SliceInner._(source));
+
+  Slice<E> asRef() => Slice._(_slice);
+
+  (SliceMut<E>, SliceMut<E>) splitAt(int splitIndex) =>
+      _slice._splitAt(splitIndex, SliceMut<E>._);
+}
 
 /// A read-only list that represents a portion (a "slice") of another list.
 ///
 /// This class provides a window into a `_root` list without creating a new
 /// copy of the data. It is highly efficient for accessing sub-ranges of a list.
-///
-/// The `Slice` is backed by a root list, and any changes made to the `Slice`
-/// (via the `[]=` operator) will modify the underlying `_root` list.
-class Slice<E> extends ListMixin<E> with UnmodifiableLengthList<E> {
-  /// Internal constructor for creating a slice.
-  ///
-  /// This constructor is used to create a new `Slice` that refers to a
-  /// specific range of the [_root] list. The `_start` and `length` define
-  /// the boundaries of the slice.
-  Slice._(this._root, [int start = 0, int? end])
-      : _start = start,
-        length = (end ?? _root.length) - start;
-
-  /// Creates a `Slice` from an `Iterable`.
+@immutable
+extension type const Slice<E>._(_SliceInner<E> _slice)
+    implements _SliceInner<E> {
+  /// Creates a new slice from an iterable.
   ///
   /// This factory constructor creates a new list from the `source` and
-  /// then wraps it in a `Slice`.
-  factory Slice.from(Iterable<E> source) => Slice._([...source]);
+  /// then wraps it in a `Slice`. Since it creates a copy, changes
+  /// will not affect the original source.
+  Slice.from(Iterable<E> source) : this._(_SliceInner._([...source]));
+
+  /// Creates a new slice that directly references a list.
+  ///
+  /// This constructor is "unsafe" because it does not create a copy of
+  /// the source list. Use with caution.
+  Slice.unsafeFrom(List<E> source) : this._(_SliceInner._(source));
+
+  /// Throws an [UnsupportedError] because [Slice] is an immutable view.
+  ///
+  /// This method is overridden to prevent any modification attempts.
+  void operator []=(Never index, Never value) {
+    throw UnsupportedError(
+      '[Slice] $runtimeType can not support value updates by index',
+    );
+  }
+
+  @Deprecated('message')
+  @internal
+  Never sort(Never compare) {
+    throw UnsupportedError('[Slice] $runtimeType can not be sorted');
+  }
+
+  Slice<E> slice(int start, {int? end, int? len}) {
+    return Slice._(_slice._slice(start, end: end, len: len));
+  }
+
+  (Slice<E>, Slice<E>) splitAt(int splitIndex) =>
+      _slice._splitAt(splitIndex, Slice<E>._);
+}
+
+class _SliceInner<E> extends ListMixin<E> with UnmodifiableLengthList<E> {
+  const _SliceInner._(this._root, [int start = 0, int? end])
+      : _start = start,
+        length = (end ?? _root.length) - start;
 
   /// The underlying root list that this slice provides a view of.
   final List<E> _root;
@@ -48,14 +100,14 @@ class Slice<E> extends ListMixin<E> with UnmodifiableLengthList<E> {
   @override
   E operator [](int index) => _root[_start + index];
 
-  /// Sets the value at the given `index` within the slice.
-  ///
-  /// The change is applied directly to the underlying `_root` list.
+  // ************************ Mutable section ************************/
   @override
   void operator []=(int index, E value) {
     IndexError.check(index, length);
     _root[_start + index] = value;
   }
+
+  // ******************* Mutable/Immutable section *******************/
 
   /// Creates a new `Slice` that represents a sub-range of this slice.
   ///
@@ -71,29 +123,38 @@ class Slice<E> extends ListMixin<E> with UnmodifiableLengthList<E> {
   /// - `end`: (Optional) The exclusive end index of the new slice, relative
   ///          to the current slice.
   /// - `length`: (Optional) The length of the new slice.
-  Slice<E> slice(int start, {int? end, int? length}) {
-    if (start < 0) {
-      throw RangeError.range(start, 0, this.length, 'start');
-    }
+  _SliceInner<E> _slice(int start, {int? end, int? len}) {
+    assert(
+      end == null || len == null,
+      'end and len can not be non-null at the same time',
+    );
+    RangeError.checkValueInInterval(start, 0, length, 'start');
+    final newStart = _start + start;
 
-    if (end != null && end < 0) {
-      throw RangeError.range(end, 0, this.length, 'end');
-    }
-
-    if (length != null && length < 0) {
-      throw RangeError.range(length, 0, this.length, 'length');
-    }
-
-    final newStart = this._start + start;
     if (end != null) {
-      return Slice._(_root, newStart, this._start + end);
+      RangeError.checkValueInInterval(end, newStart, length, 'end');
+      final newEnd = this._start + end;
+      return _SliceInner._(_root, newStart, newEnd);
     }
 
-    if (length != null) {
-      final newEnd = newStart + length;
-      return Slice._(_root, newStart, newEnd);
+    if (len != null) {
+      final maxLen = length - start;
+      RangeError.checkValueInInterval(len, 0, maxLen, 'len');
+      final newEnd = newStart + len;
+      return _SliceInner._(_root, newStart, newEnd);
     }
 
-    return Slice._(_root, newStart, this._start + this.length);
+    return _SliceInner._(_root, newStart, this._start + this.length);
+  }
+
+  (T, T) _splitAt<T>(
+    int splitIndex,
+    T Function(_SliceInner<E>) map,
+  ) {
+    RangeError.checkValueInInterval(splitIndex, 0, length, 'splitIndex');
+    return (
+      map(_SliceInner._(_root, _start, _start + splitIndex)),
+      map(_SliceInner._(_root, _start + splitIndex, _start + length)),
+    );
   }
 }
--- flutils/lib/src/forms/forms.dart
+++ flutils/lib/src/forms/forms.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/widgets.dart';
+
 export 'password_editing_controller.dart';
+
+late PlaceholderSpan a;
+late WidgetSpan b;
